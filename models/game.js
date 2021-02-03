df462337e76f5748a8d9d26b91d3d97edc50b943
angela <angelagongli@gmail.com>
2021-02-03 17:59:42 -0600

    Update Game model in game.js

--- models/game.js
+++ models/game.js
@@ -2,10 +2,28 @@ module.exports = function(sequelize, DataTypes) {
     const Game = sequelize.define("Game", {
         // We can name the game, e.g. "Dad plays me with handicap on Saturday":
         name: DataTypes.STRING,
-        startDate: DataTypes.DATE,
-        isOngoing: DataTypes.BOOLEAN,
+        startDate: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: sequelize.fn('NOW')
+        },
+        isOngoing: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: true
+        },
         winner: DataTypes.ENUM("Dad", "Me"),
-        loser: DataTypes.ENUM("Dad", "Me")
+        loser: DataTypes.ENUM("Dad", "Me"),
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: sequelize.fn('NOW')
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: sequelize.fn('NOW')
+        }
     });
 
     Game.associate = function(models) {
