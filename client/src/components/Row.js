4a1d0a13bc6d0f797fa53c961c34721baf86f62a
angela <angelagongli@gmail.com>
2021-02-03 20:59:23 -0600

    Update Row.js

--- client/src/components/Row.js
+++ client/src/components/Row.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Paper } from '@material-ui/core';
+import { Paper, Avatar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
@@ -11,6 +11,24 @@ const useStyles = makeStyles(() => ({
         width: "100%",
         height: 100,
         backgroundColor: "lightcyan"
+    },
+    piece: {
+        width: "100%",
+        height: "100%",
+        fontFamily: "fangsong",
+        fontWeight: "bold",
+        fontSize: 32,
+        borderWidth: 1,
+        borderStyle: "solid",
+        backgroundColor: "lightyellow"
+    },
+    DadPiece: {
+        color: "black",
+        borderColor: "black"
+    },
+    myPiece: {
+        color: "red",
+        borderColor: "red"
     }
 }));
 
@@ -30,7 +48,11 @@ function Row(props) {
             {props.pieces.map((piece, index) => (
                 <div name={`r${props.rowIndex}c${index}`} className="col-1">
                     { piece ?
-                    <Paper id={`r${props.rowIndex}c${index}`} elevation={10} className={classes.root} onClick={handleClick} onMouseEnter={handleHover} square>{piece}</Paper> :
+                    <Paper id={`r${props.rowIndex}c${index}`} elevation={10} className={classes.root} onClick={handleClick} onMouseEnter={handleHover} square>
+                        <Avatar className={`${classes.piece} ${piece.side === "Dad" ? classes.DadPiece : classes.myPiece}`}>
+                            {piece.character}
+                        </Avatar>
+                    </Paper> :
                     <Paper id={`r${props.rowIndex}c${index}`} className={classes.empty} onClick={handleClick} square />}
                 </div>
             ))}
