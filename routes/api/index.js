f61d6f5337538b3a2ddcf81f598d69478b72e8db
angela <angelagongli@gmail.com>
2021-02-07 08:59:23 -0600

    Update API routes index.js with Move routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const pieceRoutes = require("./pieces");
+const moveRoutes = require("./moves");
 
 router.use("/pieces", pieceRoutes);
+router.use("/moves", moveRoutes);
 
 module.exports = router;
