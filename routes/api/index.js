4fb580595d01d2fe2d37939c9ffdd47aac3870a9
angela <angelagongli@gmail.com>
2021-02-07 11:49:24 -0600

    Update API routes index.js with Game routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const pieceRoutes = require("./pieces");
 const moveRoutes = require("./moves");
+const gameRoutes = require("./games");
 
 router.use("/pieces", pieceRoutes);
 router.use("/moves", moveRoutes);
+router.use("/games", gameRoutes);
 
 module.exports = router;
