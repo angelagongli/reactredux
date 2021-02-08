791001fa43783c18439564e928eab7dab90421fc
angela <angelagongli@gmail.com>
2021-02-07 20:48:20 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Board from "./components/Board";
+import MoveHistory from "./components/MoveHistory";
+import GameHistory from "./components/GameHistory";
 import "./assets/css/style.css";
 
 function App() {
   return (
     <div className="App">
       <Board />
+      <MoveHistory />
+      <GameHistory />
     </div>
   );
 }
