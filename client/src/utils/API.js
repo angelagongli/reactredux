0f2f01a8e8ba2b3b5dcd28ec80a416dc8405d581
angela <angelagongli@gmail.com>
2021-02-07 19:35:08 -0600

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,7 +7,22 @@ export default {
     updatePiece: function(id, update) {
         return axios.put("/api/pieces/" + id, update);
     },
+    getMovesAll: function() {
+        return axios.get("/api/moves");
+    },
+    getMovesAllByGame: function(id) {
+        return axios.get("/api/moves/" + id);
+    },
     makeMove: function(move) {
         return axios.post("/api/moves", move);
+    },
+    getGamesAll: function() {
+        return axios.get("/api/games");
+    },
+    makeGame: function(game) {
+        return axios.post("/api/games", game);
+    },
+    updateGame: function(id, update) {
+        return axios.put("/api/games/" + id, update);
     }
 };
