340c2f5278d7aad226abe033edb045504a0fee1f
angela <angelagongli@gmail.com>
2021-02-17 18:54:37 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from "./components/Board";
 import MoveHistory from "./components/MoveHistory";
 import GameHistory from "./components/GameHistory";
 import "./assets/css/style.css";
 
 function App() {
+  const [moveToReturn, setMoveToReturn] = useState({});
+  const [moveSubmission, setMoveSubmission] = useState({});
+  
+  function pullMoveSubmission(move) {
+    console.log("Move submission: " + JSON.stringify(move));
+    setMoveSubmission(move);
+  }
+  
+  function pullMoveToReturn(move) {
+    console.log("Move to return: " + JSON.stringify(move));
+    setMoveToReturn(move);
+  }
+  
   return (
     <div className="App">
-      <Board />
-      <MoveHistory />
+      <Board pullMoveSubmission={pullMoveSubmission} moveToReturn={moveToReturn} />
+      <MoveHistory pullMoveToReturn={pullMoveToReturn} moveSubmission={moveSubmission} />
       <GameHistory />
     </div>
   );
